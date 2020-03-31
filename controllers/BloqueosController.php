75042c44da58746bace3955361549009fc79ce4f
alfredobp <alfredo.barragan@iesdonana.org>
2020-03-31 10:06:18 +0200

    Actualizo los controladores para controlar el acceso a ciertas acciones que deben ser realizadas por usuarios autorizados

--- controllers/BloqueosController.php
+++ controllers/BloqueosController.php
@@ -5,6 +5,7 @@ namespace app\controllers;
 use Yii;
 use app\models\Bloqueos;
 use app\models\BloqueosSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
