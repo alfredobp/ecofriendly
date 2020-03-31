507f1ed31e1fc0273959fc42ef48dd444a52597b
alfredobp <alfredo.barragan@iesdonana.org>
2020-03-31 19:44:56 +0200

    modifico etiquetas en login.php

--- views/site/login.php
+++ views/site/login.php
@@ -10,7 +10,7 @@ use yii\bootstrap4\ActiveForm;
 ?>
 
 <div class="container2">
-    <div class="site-login">
+    <main class="site-login">
         <h1><?= Html::encode($this->title) ?></h1>
 
 
@@ -19,74 +19,74 @@ use yii\bootstrap4\ActiveForm;
             'id' => 'login-form',
             'layout' => 'horizontal',
             'fieldConfig' => [
-                'horizontalCssClasses' => ['wrapper' => 'col-sm-8'],
+                'horizontalCssClasses' => ['wrapper' => 'col-sm-12 col-md-10'],
             ],
         ]); ?>
 
-            <h1>EcoFriendly:<p><em> en busca de la sostenibilidad</em></h1>
+        <h1>EcoFriendly:<p><em> en busca de la sostenibilidad</em></h1>
 
-            <div class="container-fluid">
-                <div class="d-flex justify-content-center h-100">
-                    <div class="card col-6">
-                        <div class="card-header">
-                            <h3>Inicio de sesión</h3>
 
+        <div class="d-flex justify-content-center h-100">
+            <div class="card col-6">
+                <div class="card-header">
+                    <h3>Inicio de sesión</h3>
+
+                </div>
+                <div class="card-body col-12">
+
+                    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+                    <?= $form->field($model, 'password')->passwordInput() ?>
+
+                    <div class="form-group">
+                        <div class="offset-sm-2">
+                            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
                         </div>
-                        <div class="card-body">
-
-                            <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                            <?= $form->field($model, 'password')->passwordInput() ?>
-
-                            <div class="form-group">
-                                <div class="offset-sm-2">
-                                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- <div class="row align-items-center remember">
+                    </div>
+                </div>
+                <!-- <div class="row align-items-center remember">
                             <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
 
                         </div> -->
-                        <br>
-                        <div class="card-footer">
-                            <div class="d-flex justify-content-center links">
-                                ¿Todavía sin cuenta?<a href="index.php?r=usuarios%2Fregistrar">Date de alta</a>
-                            </div>
-                            <div class="d-flex justify-content-center">
-                                <a href="index.php?r=usuarios%2Frecoverpass">¿Olvídaste tu contraseña?</a>
-                            </div>
-                            <?php ActiveForm::end(); ?>
-                        </div>
+                <br>
+                <div class="card-footer">
+                    <div class="d-flex justify-content-center links">
+                        ¿Todavía sin cuenta?<a href="index.php?r=usuarios%2Fregistrar">Date de alta</a>
                     </div>
+                    <div class="d-flex justify-content-center">
+                        <a href="index.php?r=usuarios%2Frecoverpass">¿Olvídaste tu contraseña?</a>
+                    </div>
+                    <?php ActiveForm::end(); ?>
                 </div>
-            </div>
-
-            <div class="bird-container bird-container--one">
-                <div class="bird bird--one"></div>
-            </div>
-
-            <div class="bird-container bird-container--two">
-                <div class="bird bird--two"></div>
-            </div>
-
-            <div class="bird-container bird-container--three">
-                <div class="bird bird--three"></div>
-            </div>
-
-            <div class="bird-container bird-container--four">
-                <div class="bird bird--four"></div>
             </div>
 
         </div>
 
+        <div class="bird-container bird-container--one">
+            <div class="bird bird--one"></div>
+        </div>
+
+        <div class="bird-container bird-container--two">
+            <div class="bird bird--two"></div>
+        </div>
+
+        <div class="bird-container bird-container--three">
+            <div class="bird bird--three"></div>
+        </div>
+
+        <div class="bird-container bird-container--four">
+            <div class="bird bird--four"></div>
+        </div>
+
+    </main>
 
 
 
 
-    </div>
 
-    <?php
+</div>
+
+<?php
 
     /* @var $this yii\web\View */
