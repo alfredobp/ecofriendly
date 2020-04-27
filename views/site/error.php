5320e2aa203074284faa7cbec8cccb8ae80269f8
Alfredo Barragán <alfredo.barragan@iesdonana.org>
2020-04-27 22:50:05 +0200

    [FIX] Refactorización

--- views/site/error.php
+++ views/site/error.php
@@ -18,20 +18,10 @@ $this->registerCssFile('@web/css/error.css');
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-    <title>404 HTML Template by Colorlib</title>
+    <title>Página error #ecofriendly</title>
 
-    <!-- Google font -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,700" rel="stylesheet">
 
-    <!-- Custom stlylesheet -->
-    <link type="text/css" rel="stylesheet" href="css/style.css" />
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-		<![endif]-->
 
 </head>
 
@@ -44,8 +34,8 @@ $this->registerCssFile('@web/css/error.css');
                     <h1>Oops!</h1>
                     <h2><?= nl2br(Html::encode($message)) ?></h2>
                 </div>
-                <a href="/INDEX">Ir  a la página principal</a>
+                <a href="/index">Ir  a la página principal</a>
             </div>
         </div>
     </div>
-</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</body>
